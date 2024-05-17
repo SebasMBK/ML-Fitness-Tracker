a5256c1dad1a24b9c4648b48a932eaa74e3f1b7d
Sebastian Kassim Montenegro Bravo <sebastian.montenegro@globant.com>
2024-05-17 12:56:04 -0500

    moved functions to other folder

--- src/data_processing/merge_transform.py
+++ src/data_processing/merge_transform.py
@@ -1,31 +1,5 @@
 import pandas as pd
-from ..common_functions.data_common_functions import read_sql_table, create_id, incremental_insert
-
-def resample_frequency(df: pd.DataFrame) -> pd.DataFrame:
-    # Sampling rule
-    sampling_rule = {
-        'x_axis_g': 'mean',
-        'y_axis_g': 'mean',
-        'z_axis_g': 'mean',
-        'category': 'last',
-        'label': 'last',
-        'participant': 'last',
-        'x_axis_deg_s': 'mean',
-        'y_axis_deg_s': 'mean',
-        'z_axis_deg_s': 'mean',
-        'set': 'last'
-    }
-    # We have to use a frequency that gives us a good amount of data,
-    # but not too much that becomes too expensive to compute
-    time_rule = '200ms'
-
-    # The dataframe will become to large if we resample everything in one go.
-    # So we are going to group by day (this is possible because the index is a date),
-    # and perfom the sampling in each of those groups and then concat everything
-    days = [group for i, group in df.groupby(pd.Grouper(freq='D'))] # This is a collection of dataframes
-    df_resampled = pd.concat([df.resample(rule=time_rule).apply(sampling_rule).dropna() for df in days])
-    df_resampled["id"] = df_resampled.apply(create_id, axis=1).astype("string")
-    return df_resampled
+from ..common_functions.data_common_functions import read_sql_table, incremental_insert, resample_frequency
 
 if __name__ == '__main__':
     # Read accelerometer data
