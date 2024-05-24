bdb35e1e55473d362cc38ee77ae402d55930fb79
Sebastian Kassim Montenegro Bravo <sebastian.montenegro@globant.com>
2024-05-24 12:07:43 -0500

    added code for feature engineering and inserting into table

--- src/data_processing/build_features.py
+++ src/data_processing/build_features.py
@@ -1,7 +1,8 @@
-from ..common_functions.feature_engineering_functions import LowPassFilter, PrincipalComponentAnalysis, NumericalAbstraction
-from ..common_functions.data_common_functions import read_sql_table
-import matplotlib.pyplot as plt
+from ..common_functions.feature_engineering_functions import FourierTransformation, LowPassFilter, PrincipalComponentAnalysis, NumericalAbstraction
+from ..common_functions.data_common_functions import read_sql_table, full_load
 import numpy as np
+import pandas as pd
+from sklearn.cluster import KMeans
 
 if __name__ == '__main__':
     # Load the data
@@ -79,6 +80,62 @@ if __name__ == '__main__':
     gyr_r = df_squared["x_axis_deg_s"] ** 2 + df_squared["y_axis_deg_s"] ** 2 + df_squared["z_axis_deg_s"] ** 2
     df_squared["acc_r"] = np.sqrt(acc_r)
     df_squared["gyr_r"] = np.sqrt(gyr_r)
+
+    # Calculating the rolling average for the dataset to obtain more data from the dataset
+    # similar to window functions
+    df_rolling = df_squared.copy()
+    numabs = NumericalAbstraction()
+    window_size = int(1000/200)
+    predictor_columns = predictor_columns + ["acc_r", "gyr_r"]
+
+    # A subset of the data is needed to not mix different sets data
+    df_rolling_list = []
+    for set in df_rolling["set"].unique():
+        subset = df_rolling[df_rolling["set"] == set].copy()
+        for col in predictor_columns:
+            subset = numabs.abstract_numerical(subset, [col], window_size, "mean")
+            subset = numabs.abstract_numerical(subset, [col], window_size, "std")
+        df_rolling_list.append(subset)
+    df_rolling = pd.concat(df_rolling_list)
+
+    # Frequency abstraction. Usefull to obtain insights and components from
+    # frequency data
+    df_frequency = df_rolling.copy().reset_index()
+    freqabs = FourierTransformation()
+    frequency = int(1000/200)
+    window_size = int(2800/200)
+    df_frequency_list = []
+    for s in df_frequency["set"].unique():
+        subset = df_frequency[df_frequency["set"] == s].reset_index(drop=True).copy()
+        subset = freqabs.abstract_frequency(subset, predictor_columns, window_size, frequency)
+        df_frequency_list.append(subset)
+    df_frequency = pd.concat(df_frequency_list).set_index("epoch_ms", drop=True)
+
+    # Dealing with overlapping windows to avoid overfiting
+    df_frequency = df_frequency.dropna()
+    # To avoid overlaping, 50% of the data will be dropped by skipping every other row
+    df_frequency = df_frequency.iloc[::2]
+
+    # Clustering
+    df_cluster = df_frequency.copy()
+    cluster_columns = ["x_axis_g", "y_axis_g", "z_axis_g"]
+    k = 5 # This is obtained using the inertias with the elbow method
+    kmeans = KMeans(n_clusters=k, n_init=20, random_state=0)
+    subset = df_cluster[cluster_columns]
+    df_cluster["cluster"] = kmeans.fit_predict(subset)
+
+    # Insert into table
+    full_load(
+        df=df_cluster,
+        table_schema="clean",
+        table_name="fitness_tracker",
+        username="postgres",
+        password="postgres",
+        hostname="localhost",
+        port=5432,
+        database="ml-fitness-tracker"
+    )
+
 
 
 
